3d8fd79dd7b1328d661a1f8f8ee72dbe8a012e61
Nisal Balasooriya <nisalavishka2001@gmail.com>
2025-04-08 14:55:55 +0530

    Products Router BoilerPlate

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import ProductsRoutes from './routes/products/index'
 
 const app = express();
 const port = 3000;
@@ -6,6 +7,8 @@ const port = 3000;
 app.get('/', (req, res) => {
     res.send('Hello World!');
 })
+
+app.use('/api/v1/products', ProductsRoutes);
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
